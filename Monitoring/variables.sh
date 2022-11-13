efa7db16436b6f785898a97597c3bcf685203f5b
NktSH <49846495+NktSH@users.noreply.github.com>
2022-11-14 00:33:36 +0300

    Update variables.sh

--- Monitoring/variables.sh
+++ Monitoring/variables.sh
@@ -1,5 +1,5 @@
-#DeFund monitoring variables template 
+#Nibiru monitoring variables template 
 COS_BIN_NAME=insert_path_to_nibid_binary # example: /root/go/bin/nibid or /home/user/go/bin/nibid
 COS_PORT_RPC=insert_port_here              # default: 26657
 COS_VALOPER=insert_valoper_address         # example: nibivaloper1234545636767376535673
-LOGENTRY=insert_project_name		 # example: nibiru
+LOGENTRY=nibiru
