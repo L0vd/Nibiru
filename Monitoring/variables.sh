4e40a17ed3cd59c39ae01e91b3b33577baee22fe
NktSH <49846495+NktSH@users.noreply.github.com>
2022-11-14 00:28:19 +0300

    Add files via upload

---
+++ Monitoring/variables.sh
@@ -0,0 +1,5 @@
+#Nibiru monitoring variables template 
+COS_BIN_NAME=/usr/local/bin/nibid             # example: /root/go/bin/nibid or /home/user/go/bin/nibid
+COS_PORT_RPC=28657         # default: 26657
+COS_VALOPER=nibivaloper1lc6ycz07u3zvhcrgzxmuz50ua3q07kfh9jsg9p           # example: nibivaloper1234545636767376535673
+LOGENTRY=nibiru
